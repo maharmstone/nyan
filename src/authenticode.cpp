3084fe49d11bbd131cbf850e6f3d7c7bb44b21c2
Mark Harmstone <mark@harmstone.com>
2024-03-18 16:21:42 +0000

    add calc_authenticode

--- src/authenticode.cpp
+++ src/authenticode.cpp
@@ -1,5 +1,51 @@
-int main() {
+#include <sys/mman.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdint.h>
+#include <iostream>
+#include <span>
+
+using namespace std;
+
+static void authenticode(span<const uint8_t> file) {
     // FIXME
+}
+
+static void calc_authenticode(const char* fn) {
+    int fd = open(fn, O_RDONLY);
+
+    if (fd == -1)
+        throw runtime_error("open failed (errno " + to_string(errno) + ")");
+
+    size_t length = 0x1000; // FIXME
+
+    void* addr = mmap(nullptr, length, PROT_READ, MAP_PRIVATE, fd, 0);
+    if (addr == MAP_FAILED) {
+        auto err = errno;
+        close(fd);
+        throw runtime_error("mmap failed (errno " + to_string(err) + ")");
+    }
+
+    try {
+        authenticode(span((uint8_t*)addr, length));
+    } catch (...) {
+        munmap(addr, length);
+        close(fd);
+        throw;
+    }
+
+    munmap(addr, length);
+    close(fd);
+}
+
+int main() {
+    // FIXME - parse arguments
+
+    try {
+        calc_authenticode("/home/nobackup/btrfs-package/1.9/release/amd64/mkbtrfs.exe");
+    } catch (const exception& e) {
+        cerr << "Exception: " << e.what() << endl;
+    }
 
     return 0;
 }
