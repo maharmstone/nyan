ddd7df8734a57801a4cb391827a1fdf0f1ac79b9
Mark Harmstone <mark@harmstone.com>
2024-03-18 16:33:24 +0000

    calc_authenticode: parse PE headers

--- src/authenticode.cpp
+++ src/authenticode.cpp
@@ -1,14 +1,48 @@
 #include <sys/mman.h>
+#include <sys/stat.h>
 #include <fcntl.h>
 #include <unistd.h>
 #include <stdint.h>
 #include <iostream>
 #include <span>
+#include "pe.h"
 
 using namespace std;
 
-static void authenticode(span<const uint8_t> file) {
+template<typename T>
+static void authenticode2(span<const uint8_t> file, const T& opthead) {
     // FIXME
+}
+
+static void authenticode(span<const uint8_t> file) {
+    if (file.size() < sizeof(IMAGE_DOS_HEADER))
+        throw runtime_error("File too short for IMAGE_DOS_HEADER.");
+
+    auto& dos_header = *(const IMAGE_DOS_HEADER*)file.data();
+
+    if (dos_header.e_magic != IMAGE_DOS_SIGNATURE)
+        throw runtime_error("Invalid DOS signature.");
+
+    auto& nt_header = *(const IMAGE_NT_HEADERS*)(file.data() + dos_header.e_lfanew);
+
+    if (file.size() < dos_header.e_lfanew + sizeof(IMAGE_NT_HEADERS))
+        throw runtime_error("File too short for IMAGE_NT_HEADERS.");
+
+    if (nt_header.Signature != IMAGE_NT_SIGNATURE)
+        throw runtime_error("Incorrect PE signature.");
+
+    switch (nt_header.OptionalHeader32.Magic) {
+        case IMAGE_NT_OPTIONAL_HDR32_MAGIC:
+            authenticode2(file, nt_header.OptionalHeader32);
+            break;
+
+        case IMAGE_NT_OPTIONAL_HDR64_MAGIC:
+            authenticode2(file, nt_header.OptionalHeader64);
+            break;
+
+        default:
+            throw runtime_error("Invalid optional header magic.");
+    }
 }
 
 static void calc_authenticode(const char* fn) {
@@ -17,7 +51,15 @@ static void calc_authenticode(const char* fn) {
     if (fd == -1)
         throw runtime_error("open failed (errno " + to_string(errno) + ")");
 
-    size_t length = 0x1000; // FIXME
+    struct stat st;
+
+    if (fstat(fd, &st) == -1) {
+        auto err = errno;
+        close(fd);
+        throw runtime_error("fstat failed (errno " + to_string(err) + ")");
+    }
+
+    size_t length = st.st_size;
 
     void* addr = mmap(nullptr, length, PROT_READ, MAP_PRIVATE, fd, 0);
     if (addr == MAP_FAILED) {
---
+++ src/pe.h
@@ -0,0 +1,120 @@
+#pragma once
+
+static const uint16_t IMAGE_DOS_SIGNATURE = 0x5a4d; // "MZ"
+static const uint32_t IMAGE_NT_SIGNATURE = 0x00004550; // "PE\0\0"
+
+static const uint16_t IMAGE_NT_OPTIONAL_HDR32_MAGIC = 0x10b;
+static const uint16_t IMAGE_NT_OPTIONAL_HDR64_MAGIC = 0x20b;
+
+struct IMAGE_DOS_HEADER {
+    uint16_t e_magic;
+    uint16_t e_cblp;
+    uint16_t e_cp;
+    uint16_t e_crlc;
+    uint16_t e_cparhdr;
+    uint16_t e_minalloc;
+    uint16_t e_maxalloc;
+    uint16_t e_ss;
+    uint16_t e_sp;
+    uint16_t e_csum;
+    uint16_t e_ip;
+    uint16_t e_cs;
+    uint16_t e_lfarlc;
+    uint16_t e_ovno;
+    uint16_t e_res[4];
+    uint16_t e_oemid;
+    uint16_t e_oeminfo;
+    uint16_t e_res2[10];
+    uint32_t e_lfanew;
+};
+
+struct IMAGE_FILE_HEADER {
+    uint16_t Machine;
+    uint16_t NumberOfSections;
+    uint32_t TimeDateStamp;
+    uint32_t PointerToSymbolTable;
+    uint32_t NumberOfSymbols;
+    uint16_t SizeOfOptionalHeader;
+    uint16_t Characteristics;
+};
+
+struct IMAGE_DATA_DIRECTORY {
+    uint32_t VirtualAddress;
+    uint32_t Size;
+};
+
+struct IMAGE_OPTIONAL_HEADER32 {
+    uint16_t Magic;
+    uint8_t MajorLinkerVersion;
+    uint8_t MinorLinkerVersion;
+    uint32_t SizeOfCode;
+    uint32_t SizeOfInitializedData;
+    uint32_t SizeOfUninitializedData;
+    uint32_t AddressOfEntryPoint;
+    uint32_t BaseOfCode;
+    uint32_t BaseOfData;
+    uint32_t ImageBase;
+    uint32_t SectionAlignment;
+    uint32_t FileAlignment;
+    uint16_t MajorOperatingSystemVersion;
+    uint16_t MinorOperatingSystemVersion;
+    uint16_t MajorImageVersion;
+    uint16_t MinorImageVersion;
+    uint16_t MajorSubsystemVersion;
+    uint16_t MinorSubsystemVersion;
+    uint32_t Win32VersionValue;
+    uint32_t SizeOfImage;
+    uint32_t SizeOfHeaders;
+    uint32_t CheckSum;
+    uint16_t Subsystem;
+    uint16_t DllCharacteristics;
+    uint32_t SizeOfStackReserve;
+    uint32_t SizeOfStackCommit;
+    uint32_t SizeOfHeapReserve;
+    uint32_t SizeOfHeapCommit;
+    uint32_t LoaderFlags;
+    uint32_t NumberOfRvaAndSizes;
+    IMAGE_DATA_DIRECTORY DataDirectory[0];
+};
+
+struct IMAGE_OPTIONAL_HEADER64 {
+    uint16_t Magic;
+    uint8_t MajorLinkerVersion;
+    uint8_t MinorLinkerVersion;
+    uint32_t SizeOfCode;
+    uint32_t SizeOfInitializedData;
+    uint32_t SizeOfUninitializedData;
+    uint32_t AddressOfEntryPoint;
+    uint32_t BaseOfCode;
+    uint64_t ImageBase;
+    uint32_t SectionAlignment;
+    uint32_t FileAlignment;
+    uint16_t MajorOperatingSystemVersion;
+    uint16_t MinorOperatingSystemVersion;
+    uint16_t MajorImageVersion;
+    uint16_t MinorImageVersion;
+    uint16_t MajorSubsystemVersion;
+    uint16_t MinorSubsystemVersion;
+    uint32_t Win32VersionValue;
+    uint32_t SizeOfImage;
+    uint32_t SizeOfHeaders;
+    uint32_t CheckSum;
+    uint16_t Subsystem;
+    uint16_t DllCharacteristics;
+    uint64_t SizeOfStackReserve;
+    uint64_t SizeOfStackCommit;
+    uint64_t SizeOfHeapReserve;
+    uint64_t SizeOfHeapCommit;
+    uint32_t LoaderFlags;
+    uint32_t NumberOfRvaAndSizes;
+    IMAGE_DATA_DIRECTORY DataDirectory[0];
+};
+
+struct IMAGE_NT_HEADERS {
+    uint32_t Signature;
+    IMAGE_FILE_HEADER FileHeader;
+    union {
+        IMAGE_OPTIONAL_HEADER32 OptionalHeader32;
+        IMAGE_OPTIONAL_HEADER64 OptionalHeader64;
+    };
+};
