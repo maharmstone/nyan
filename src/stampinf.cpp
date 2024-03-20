ee05ff0f6a4b5de9a6584019400a124090fe7510
Mark Harmstone <mark@harmstone.com>
2024-03-20 20:55:36 +0000

    stampinf: write to file

--- src/stampinf.cpp
+++ src/stampinf.cpp
@@ -42,130 +42,138 @@ static bool is_whitespace(string_view sv) {
 static void stampinf(const filesystem::path& fn, string_view ver_section,
                      const optional<chrono::year_month_day>& date,
                      const optional<version>& ver) {
-    ifstream f(fn);
+    vector<string> lines;
+
+    {
+        ifstream f(fn);
+
+        // FIXME - throw more descriptive error message (not found, access denied, etc.)
+        if (!f.is_open())
+            throw runtime_error("Could not open " + fn.string() + " for reading.");
+
+        unsigned int line_no = 0;
+        string section;
+        bool changed_driverver = false;
+
+        while (!f.eof()) {
+            string line;
+            line_no++;
+
+            getline(f, line);
+
+            if (line.front() == '[') {
+                auto end = line.find(']');
+
+                if (end == string::npos)
+                    throw runtime_error("Line " + to_string(line_no) + ": square brackets not terminated.");
+
+                if (!changed_driverver && date.has_value() && section == ver_section) {
+                    vector<string> ws_lines;
+
+                    while (!lines.empty() && is_whitespace(lines.back())) {
+                        ws_lines.emplace_back(lines.back());
+                        lines.pop_back();
+                    }
+
+                    lines.emplace_back(format("DriverVer = {:02}/{:02}/{:04},{}.{}.{}.{}", (unsigned int)date->month(), (unsigned int)date->day(), (int)date->year(),
+                                            ver->major, ver->minor, ver->build, ver->revision));
+
+                    while (!ws_lines.empty()) {
+                        lines.push_back(ws_lines.back());
+                        ws_lines.pop_back();
+                    }
+                }
+
+                section = lcstring(string_view(line.data() + 1, end - 1));
+            }
+
+            auto sv = string_view(line);
+
+            while (!sv.empty() && (sv.front() == ' ' || sv.front() == '\t')) {
+                sv = sv.substr(1);
+            }
+
+            bool in_quotes = false;
+            for (size_t i = 0; i < sv.size(); i++) {
+                if (sv[i] == '"')
+                    in_quotes = !in_quotes;
+                else if (sv[i] == ';' && !in_quotes) {
+                    sv = sv.substr(0, i);
+                    break;
+                }
+            }
+
+            while (!sv.empty() && (sv.back() == ' ' || sv.back() == '\t' || sv.back() == '\r')) {
+                sv = sv.substr(0, sv.size() - 1);
+            }
+
+            if (sv.empty()) {
+                lines.emplace_back(line);
+                continue;
+            }
+
+            // FIXME - backslash can be used as line continuator
+
+            auto eq = sv.find('=');
+
+            if (eq == string::npos) {
+                lines.emplace_back(line);
+                continue;
+            }
+
+            string_view name, value;
+
+            name = sv.substr(0, eq);
+            value = sv.substr(eq + 1);
+
+            while (!name.empty() && (name.back() == ' ' || name.back() == '\t')) {
+                name = name.substr(0, name.size() - 1);
+            }
+
+            while (!value.empty() && (value.front() == ' ' || value.front() == '\t')) {
+                value = value.substr(1);
+            }
+
+            if (date.has_value() && section == ver_section) {
+                auto namelc = lcstring(name);
+
+                if (namelc == "driverver") {
+                    lines.emplace_back(format("DriverVer = {:02}/{:02}/{:04},{}.{}.{}.{}", (unsigned int)date->month(), (unsigned int)date->day(), (int)date->year(),
+                                            ver->major, ver->minor, ver->build, ver->revision));
+                    changed_driverver = true;
+                    continue;
+                }
+            }
+
+            lines.emplace_back(line);
+        }
+
+        if (!changed_driverver && date.has_value() && section == ver_section) {
+            vector<string> ws_lines;
+
+            while (!lines.empty() && is_whitespace(lines.back())) {
+                ws_lines.emplace_back(lines.back());
+                lines.pop_back();
+            }
+
+            lines.emplace_back(format("DriverVer = {:02}/{:02}/{:04},{}.{}.{}.{}", (unsigned int)date->month(), (unsigned int)date->day(), (int)date->year(),
+                                    ver->major, ver->minor, ver->build, ver->revision));
+
+            while (!ws_lines.empty()) {
+                lines.push_back(ws_lines.back());
+                ws_lines.pop_back();
+            }
+        }
+    }
+
+    ofstream f(fn);
 
     // FIXME - throw more descriptive error message (not found, access denied, etc.)
     if (!f.is_open())
-        throw runtime_error("Could not open " + fn.string() + " for reading.");
+        throw runtime_error("Could not open " + fn.string() + " for writing.");
 
-    unsigned int line_no = 0;
-    string section;
-    vector<string> lines;
-    bool changed_driverver = false;
-
-    while (!f.eof()) {
-        string line;
-        line_no++;
-
-        getline(f, line);
-
-        if (line.front() == '[') {
-            auto end = line.find(']');
-
-            if (end == string::npos)
-                throw runtime_error("Line " + to_string(line_no) + ": square brackets not terminated.");
-
-            if (!changed_driverver && date.has_value() && section == ver_section) {
-                vector<string> ws_lines;
-
-                while (!lines.empty() && is_whitespace(lines.back())) {
-                    ws_lines.emplace_back(lines.back());
-                    lines.pop_back();
-                }
-
-                lines.emplace_back(format("DriverVer = {:02}/{:02}/{:04},{}.{}.{}.{}", (unsigned int)date->month(), (unsigned int)date->day(), (int)date->year(),
-                                          ver->major, ver->minor, ver->build, ver->revision));
-
-                while (!ws_lines.empty()) {
-                    lines.push_back(ws_lines.back());
-                    ws_lines.pop_back();
-                }
-            }
-
-            section = lcstring(string_view(line.data() + 1, end - 1));
-        }
-
-        auto sv = string_view(line);
-
-        while (!sv.empty() && (sv.front() == ' ' || sv.front() == '\t')) {
-            sv = sv.substr(1);
-        }
-
-        bool in_quotes = false;
-        for (size_t i = 0; i < sv.size(); i++) {
-            if (sv[i] == '"')
-                in_quotes = !in_quotes;
-            else if (sv[i] == ';' && !in_quotes) {
-                sv = sv.substr(0, i);
-                break;
-            }
-        }
-
-        while (!sv.empty() && (sv.back() == ' ' || sv.back() == '\t' || sv.back() == '\r')) {
-            sv = sv.substr(0, sv.size() - 1);
-        }
-
-        if (sv.empty()) {
-            lines.emplace_back(line);
-            continue;
-        }
-
-        // FIXME - backslash can be used as line continuator
-
-        auto eq = sv.find('=');
-
-        if (eq == string::npos) {
-            lines.emplace_back(line);
-            continue;
-        }
-
-        string_view name, value;
-
-        name = sv.substr(0, eq);
-        value = sv.substr(eq + 1);
-
-        while (!name.empty() && (name.back() == ' ' || name.back() == '\t')) {
-            name = name.substr(0, name.size() - 1);
-        }
-
-        while (!value.empty() && (value.front() == ' ' || value.front() == '\t')) {
-            value = value.substr(1);
-        }
-
-        if (date.has_value() && section == ver_section) {
-            auto namelc = lcstring(name);
-
-            if (namelc == "driverver") {
-                lines.emplace_back(format("DriverVer = {:02}/{:02}/{:04},{}.{}.{}.{}", (unsigned int)date->month(), (unsigned int)date->day(), (int)date->year(),
-                                          ver->major, ver->minor, ver->build, ver->revision));
-                changed_driverver = true;
-                continue;
-            }
-        }
-
-        lines.emplace_back(line);
-    }
-
-    if (!changed_driverver && date.has_value() && section == ver_section) {
-        vector<string> ws_lines;
-
-        while (!lines.empty() && is_whitespace(lines.back())) {
-            ws_lines.emplace_back(lines.back());
-            lines.pop_back();
-        }
-
-        lines.emplace_back(format("DriverVer = {:02}/{:02}/{:04},{}.{}.{}.{}", (unsigned int)date->month(), (unsigned int)date->day(), (int)date->year(),
-                                  ver->major, ver->minor, ver->build, ver->revision));
-
-        while (!ws_lines.empty()) {
-            lines.push_back(ws_lines.back());
-            ws_lines.pop_back();
-        }
-    }
-
-    // FIXME - write to file
     for (const auto& l : lines) {
-        cout << l << endl;
+        f << l << endl;
     }
 }
 
