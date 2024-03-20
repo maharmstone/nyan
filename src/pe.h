54207565e901fd83d3680f69171e2b850ccf1111
Mark Harmstone <mark@harmstone.com>
2024-03-20 20:59:06 +0000

    add licence

--- src/pe.h
+++ src/pe.h
@@ -1,3 +1,20 @@
+/* Copyright (c) Mark Harmstone 2024
+ *
+ * This file is part of Nyan.
+ *
+ * Nyan is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public Licence as published by
+ * the Free Software Foundation, either version 2 of the Licence, or
+ * (at your option) any later version.
+ *
+ * Nyan is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public Licence for more details.
+ *
+ * You should have received a copy of the GNU General Public Licence
+ * along with Nyan. If not, see <https://www.gnu.org/licenses/>. */
+
 #pragma once
 
 static const uint16_t IMAGE_DOS_SIGNATURE = 0x5a4d; // "MZ"
